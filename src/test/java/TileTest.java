6e32919eee26e0944260c7732e1d35f46b7b5efa
gboumphrey <74275873+gboumphrey@users.noreply.github.com>
2024-02-20 13:19:45 +0000

    more tests

--- src/test/java/TileTest.java
+++ src/test/java/TileTest.java
@@ -6,6 +6,30 @@ public class TileTest {
     public void testCheck() {
         Tile a = new Tile();
         Assertions.assertTrue(a.check());
+        a.toggleFlagged();
+        Assertions.assertFalse(a.check());
+    }
+    @Test
+    public void testGetSet() {
+        Tile a = new Tile();
+        a.setNumber(2);
+        Assertions.assertEquals(2,a.getNumber(),"2 wasnt 2");
+    }
 
+    @Test
+    public void testFlag() {
+        Tile a = new Tile();
+        Assertions.assertEquals(1, a.toggleFlagged(), "didnt toggle");
+        Assertions.assertEquals(-1, a.toggleFlagged(), "didnt toggle");
+        a.reveal();
+        Assertions.assertEquals(0, a.toggleFlagged(), "didnt reveal");
+    }
+
+    @Test
+    public void testDraw() {
+        Tile a = new Tile();
+        Assertions.assertEquals(' ',a.drawTile(), "didnt match");
+        a.toggleFlagged();
+        Assertions.assertEquals('F', a.drawTile(), "flag didnt match");
     }
 }
